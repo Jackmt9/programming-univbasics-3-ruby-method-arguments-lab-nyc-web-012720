865df5030030eae47ea0942f319c89e9527f6eab
Jack T <jackmt9@gmail.com>
2020-01-15 01:26:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,9 +4,14 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  puts "Hi, my name is #{name, language}."
+  puts "Hi, my name is #{name}."
 end
 
+def iintroduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduction_with_language(name, language)
 
 
 
