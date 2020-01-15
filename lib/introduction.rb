3d6071b3fb32fe11d5a30870622b9f4cb9ecfef5
Jack T <jackmt9@gmail.com>
2020-01-15 01:26:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,8 +11,8 @@ def iintroduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
-def introduction_with_language(name, language)
-
+def introduction_with_language(name, language = "Ruby")
+end
 
 
 
